b13900858a4190c5222c24f225adcb22e2ce297d
Cisco Huerta <utm19010087@utma.edu.mx>
2023-06-01 08:54:22 -0600

    Carga comentada

--- src/main/java/com/vm/registro/HelloApplication.java
+++ src/main/java/com/vm/registro/HelloApplication.java
@@ -21,9 +21,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application
+{
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Inventario");
